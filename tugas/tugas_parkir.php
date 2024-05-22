e2bafe2895bda702d55119c8c6123e8ccca2d3f0
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-05-23 05:50:51 +0700

    solusi dari tugas parkir

---
+++ tugas/tugas_parkir.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tarif Parkir</title>
+</head>
+
+<body>
+  <form action="" method="get">
+    <li>
+      <label for="no_polisi">No Polisi: </label>
+      <input type="text" name="no_polisi" id="no_polisi">
+    </li>
+    <li>
+      <label for="lama_parkir">Lama Parkir: </label>
+      <input type="number" name="lama_parkir" id="lama_parkir">
+    </li>
+    <li>
+      <label for="tarif">Tarif / Jam: </label>
+      <input type="number" name="tarif" id="tarif">
+    </li>
+    <li><button name="submit">Hitung</button></li>
+  </form>
+  <?php
+  if (isset($_GET['submit'])) {
+    $hasil = $_GET['lama_parkir'] * $_GET['tarif'];
+    echo 'Kendaraan dengan No. Polisi ' . $_GET['no_polisi'] . ' dikenakan tarif parkir ' . $hasil;
+  }
+  ?>
+</body>
+
+</html>
